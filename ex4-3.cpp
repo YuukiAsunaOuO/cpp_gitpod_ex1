1700bcdf23b2bb0c04056729993a366041776acf
YuukiAsunaOuO <104436555+YuukiAsunaOuO@users.noreply.github.com>
2022-05-17 20:55:52 +0800

    Create ex4-3.cpp

---
+++ ex4-3.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main(void)
+{
+	int a;
+	while(cin>>a)
+	{
+		if((a%4==0 && a%100!=0) || a%400==0)
+		{
+			cout<<"閏年"<<endl;
+		}
+		else
+		{
+			cout<<"平年"<<endl;
+		}
+	}
+}
