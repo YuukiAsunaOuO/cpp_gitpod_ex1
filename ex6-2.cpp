c139721df05c5b8e07dd2d20004234c12875253c
YuukiAsunaOuO <104436555+YuukiAsunaOuO@users.noreply.github.com>
2022-07-01 16:47:22 +0800

    Create ex6-2.cpp

---
+++ ex6-2.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main(void)
+{
+   	int a,b,c;
+   	cin>>a>>b>>c;
+   	if(c==a+b)
+   	{
+   		cout<<"right"<<endl;
+   	}
+   	else
+   	{
+   		cout<<"error"<<endl;
+   	}
+}
