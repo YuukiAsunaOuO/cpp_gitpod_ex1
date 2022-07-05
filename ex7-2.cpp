3f43b88779b7996d17f6982b3fb395827df0f676
YuukiAsunaOuO <104436555+YuukiAsunaOuO@users.noreply.github.com>
2022-07-05 15:08:56 +0800

    Create ex7-2.cpp

---
+++ ex7-2.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+class rectangle
+{
+	public:
+	int a;
+	int b;
+	int area();
+};
+int rectangle::area()
+{
+	return (a*60)+b;
+}
+int main(void)
+{
+	int x,y;
+ 	rectangle ans;
+ 	cin>>x>>y;
+ 	ans.a=x;
+ 	ans.b=y;
+ 	cout<<ans.area()<<endl;
+}	
